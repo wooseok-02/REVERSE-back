c8b3028d43c31fb36569e0c0ac469e8b87447f17
wxdals <womgon1045@naver.com>
2026-03-29 21:10:30 +0900

    feat: Spring Boot 초기 세팅 및 .gitignore 수정

---
+++ src/test/java/com/reverse/nsu/NsuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reverse.nsu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NsuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
