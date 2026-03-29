3b7911e9597c9efada13bf201f611a7d9049a910
wxdals <womgon1045@naver.com>
2026-03-29 21:56:25 +0900

    feat: 서버 상태 확인 엔드포인트 추가 (/test)

---
+++ src/main/java/com/reverse/nsu/TestController.java
@@ -0,0 +1,30 @@
+package com.reverse.nsu;
+
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @GetMapping("/test")
+    public ResponseEntity<String> test() {
+        String html = """
+                    <!DOCTYPE html>
+                    <html lang="kr">
+                    <meta charset="UTF-8">
+                    <head>
+                        <title>서버 상태</title>
+                    </head>
+                    <body>
+                        <h1>서버 작동중 </h1>
+                    </body>
+                    </html>
+                    """;
+
+        return ResponseEntity.ok()
+                .contentType(MediaType.TEXT_HTML)
+                .body(html);
+    }
+}
